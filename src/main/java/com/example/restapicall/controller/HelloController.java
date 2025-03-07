f60e2b9af60a9e53b8c18dae2ac4f7bb5c47abe0
Bishtrishika01 <bishtrishika66@gamil.com>
2025-03-07 11:57:28 +0530

    [Rishika] Add. UC-5

--- src/main/java/com/example/restapicall/controller/HelloController.java
+++ src/main/java/com/example/restapicall/controller/HelloController.java
@@ -1,13 +1,11 @@
 package com.example.restapicall.controller;
 import org.springframework.web.bind.annotation.*;
-import com.example.restapicall.bean.User;
 @RestController // Marks this class as a REST controller
 @RequestMapping("/hello") // Base URL: http://localhost:8080/hello
 public class HelloController {
 
-    // POST request that accepts firstName and lastName in the request body
-    @PostMapping("/post")
-    public String greetUser(@RequestBody User user) {
-        return "Hello, " + user.getFirstName() + " " + user.getLastName() + "! Welcome to BridgeLabz";
+    @PutMapping("/modify/{userName}")
+    public String modifyWelcome(@PathVariable String userName, @RequestParam String familyName) {
+        return "Hello, " + userName + " " + familyName + "! Welcome to BridgeLabz";
     }
 }
