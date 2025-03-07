b73ee04186887d1e62218112f247fc8febf545f6
Bishtrishika01 <bishtrishika66@gamil.com>
2025-03-07 11:42:58 +0530

    [Rishika] Add. UC-2

--- src/main/java/com/example/restapicall/controller/HelloController.java
+++ src/main/java/com/example/restapicall/controller/HelloController.java
@@ -3,31 +3,11 @@ package com.example.restapicall.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/greet") // Base URL: http://localhost:8081/greet
 public class HelloController {
 
-    @GetMapping
-    public String sayHelloDefault() {
-        return "Hello from BridgeLabz!";
-    }
-
-    @GetMapping("/{name}")
-    public String sayHello(@PathVariable String name) {
+    @GetMapping("/query")
+    public String greetUser(@RequestParam String name) {
         return "Hello, " + name + "! Welcome to BridgeLabz";
-    }
-
-    @PostMapping
-    public String sayHelloPost(@RequestBody String name) {
-        return "Hello, " + name + "! Welcome to BridgeLabz";
-    }
-
-    @PutMapping("/{name}")
-    public String sayHelloPut(@PathVariable String name) {
-        return "Updated Hello, " + name + "!";
-    }
-
-    @DeleteMapping("/{name}")
-    public String sayHelloDelete(@PathVariable String name) {
-        return "Goodbye, " + name + "! See you again!";
     }
 }
